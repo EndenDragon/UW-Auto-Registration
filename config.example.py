cc8db2d25fcf3e9b5905703e4ed663dbacdfcb1d
Jeremy "EndenDragon" Zhang <jeremyzhang5000@gmail.com>
2016-11-19 15:36:08 -0800

    Update example config
    SLN are numbers, not strings.

--- config.example.py
+++ config.example.py
@@ -9,7 +9,7 @@ config = {
     # somehow don't meet the requirements set by UW.
     # Format the list as follows {'sln': 'addcode'} for each element (ordering it in your preference).
     # Entering a blank addcode string = no add code
-    'sln-list': [{14657: ''}, {10113: '10764', 17086: '', 17090: '', '12981': '', '13000': ''}],
+    'sln-list': [{14657: ''}, {10113: '10764', 17086: '', 17090: '', 12981: '', 13000: ''}],
 
     # The datetime of when to submit registration sln
     'registration-datetime': 'Jan 1 1979 1:33PM',
